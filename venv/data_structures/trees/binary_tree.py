97bd0153a731041878b212220ce8d511de4f589a
kris-randen <kris.randen@gmail.com>
2021-07-31 08:13:32 +0530

    Completed preorder tree traversal with a stack and preorder construction.

--- venv/data_structures/trees/binary_tree.py
+++ venv/data_structures/trees/binary_tree.py
@@ -1,15 +1,84 @@
 from constants import *
-from node import Node, State
+from node import Node
 from stack import Stack
+from collections import deque
+from copy import deepcopy
 
 
 class BinaryTree:
-    def __init__(self, value):
-        self.root = Node(value)
+    # noinspection PyShadowingNames
+    def __init__(self, values):
+        self.root = BinaryTree.construct_preorder(values)
+
+    def get_root(self):
+        return self.root
+
+    # noinspection PyShadowingNames
+    @staticmethod
+    def construct_preorder(values):
+        if not values or not values[0]:
+            return None
+        node = Node(values[0])
+        values.popleft()
+        node.left = BinaryTree.construct_preorder(values)
+        values.popleft()
+        node.right = BinaryTree.construct_preorder(values)
+        return node
+
+
+# noinspection PyShadowingNames
+class Visitor:
+
+    def __init__(self):
+        self.visit_order = deque()
+
+    def get_visit_order(self):
+        return deepcopy(self.visit_order)
+
+    def update_order(self, node, visit=True):
+        self.visit_order.append(node.get_value())
+
+    def visit(self, node):
+        node.set_visited()
+        self.update_order(node)
+
+    def refresh(self):
+        self.visit_order.clear()
+
+
+# noinspection PyShadowingNames
+def traverse_preorder_stack(tree, visitor):
+    stack = Stack()
+    node = tree.get_root()
+    visitor.visit(node)
+    stack.push(node)
+    while stack.top():
+        node = stack.top()
+        if node.get_left() and not node.get_left_visited():
+            left = node.get_left()
+            visitor.visit(left)
+            stack.push(left)
+        elif node.get_right() and not node.get_right_visited():
+            right = node.get_right()
+            visitor.visit(right)
+            stack.push(right)
+        else:
+            stack.pop()
+    return visitor.get_visit_order()
+
+
+# noinspection PyShadowingNames
+def traverse_preorder_recursive(tree, visitor):
+    pass
 
 
 if __name__ == MAIN:
-    print('hello')
-    state = State(10)
-    state.set_visited_right()
-    print(state)
+    values = deque(['apple', 'banana', 'dates', None, None, None, 'cherry', None, None])
+    tree = BinaryTree(values)
+    visitor = Visitor()
+    traverse_preorder_stack(tree, visitor)
+    visit_order = visitor.get_visit_order()
+    print(visit_order)
+
+
+
--- venv/data_structures/trees/node.py
+++ venv/data_structures/trees/node.py
@@ -4,8 +4,24 @@ from constants import *
 class Node:
     def __init__(self, value=None):
         self.value = value
+        self.visited = False
         self.left = None
         self.right = None
+
+    def get_visited(self):
+        return self.visited
+
+    def set_visited(self, visited=True):
+        self.visited = visited
+
+    def get_left_visited(self):
+        left = self.get_left()
+        return left.get_visited() if left else None
+
+    def get_right_visited(self):
+        right = self.get_right()
+        return right.get_visited() if right else None
+
 
     def get_value(self):
         return self.value
@@ -14,63 +30,29 @@ class Node:
         self.value = value
 
 
-    def get_left_child(self):
+    def get_left(self):
         return self.left
 
-    def get_right_child(self):
+    def get_right(self):
         return self.right
 
-    def set_left_child(self, value):
+    def set_left(self, value):
         self.left = Node(value)
 
-    def set_right_child(self, value):
+    def set_right(self, value):
         self.right = Node(value)
 
 
-    def has_left_child(self):
+    def has_left(self):
         return self.left is not None
 
-    def has_right_child(self):
+    def has_right(self):
         return self.right is not None
 
 
     def __repr__(self):
-        return f'Node: value = {self.get_value()}'
-
-
-class State:
-    def __init__(self, node):
-        self.node = node
-        self.visited_left = False
-        self.visited_right = False
-
-    def get_visited_left(self):
-        return self.visited_left
-
-    def get_visited_right(self):
-        return self.visited_right
-
-    def set_visited_left(self):
-        self.visited_left = True
-
-    def set_visited_right(self):
-        self.visited_right = True
-
-
-    def __str__(self):
-        s = 'Node = ' + str(self.node)
-        s += '\n-------------\n'
-        s += 'left = ' + ('' if self.get_visited_left() else 'not ') + 'visited'
-        s += '\n-------------\n'
-        s += 'right = ' + ('' if self.get_visited_right() else 'not ') + 'visited'
-        s += '\n-------------\n'
-        return s
-
-    def __repr__(self):
-        return self.__str__()
+        return f'{self.get_value()}'
 
 
 if __name__ == MAIN:
-    state = State(10)
-    state.set_visited_right()
-    print(state)
+    pass
